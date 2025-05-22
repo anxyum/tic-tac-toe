7c3e752e142eabce0def24126349214e80bcd600
anxyum <115811968+anxyum@users.noreply.github.com>
2025-05-22 10:49:06 +0200

    saucisson

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ const $newGameScreenPickPlayerIcons = document.querySelectorAll(
 const $newGameScreenNewGameCpuPlayer = document.querySelector(
   ".new-game-screen-new-game-buttons__button"
 );
+const $newGameScreenNewGamePlayer = document.querySelector(
+  ".new-game-screen-new-game-buttons__button--secondary"
+);
 const $newGameScreen = document.querySelector(".new-game-screen");
 const $gameScreen = document.querySelector(".game-screen");
 const $gridCells = document.querySelectorAll(".game-screen-grid__cell");
@@ -22,6 +25,9 @@ const $quitGameButton = document.querySelector(
 );
 const $winnerIcon = document.querySelector(
   ".game-screen-end-game-modal__title"
+);
+const $winnerText = document.querySelector(
+  ".game-screen-end-game-modal__paragraph"
 );
 const $scoreTrackers = {
   x: document.querySelector("#score-x"),
@@ -42,10 +48,15 @@ const symbols = {
   x: '<?xml version="1.0" encoding="UTF-8"?><svg class="game-screen-header-icons__cross" width="40" height="40" fill="none" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="m27.681 1.6344c-1.1716-1.1716-3.0711-1.1716-4.2427 0l-7.4383 7.4383-7.4383-7.4383c-1.1716-1.1716-3.0711-1.1716-4.2426 0l-2.6846 2.6846c-1.1716 1.1716-1.1716 3.0711 0 4.2426l7.4383 7.4383-7.4383 7.4383c-1.1716 1.1716-1.1716 3.0711 0 4.2427l2.6846 2.6846c1.1716 1.1716 3.0711 1.1716 4.2426 0l7.4383-7.4383 7.4383 7.4383c1.1716 1.1716 3.0711 1.1716 4.2427 0l2.6846-2.6846c1.1716-1.1716 1.1716-3.0711 0-4.2427l-7.4383-7.4383 7.4383-7.4383c1.1716-1.1716 1.1716-3.0711 0-4.2426l-2.6846-2.6846z" clip-rule="evenodd" fill-rule="evenodd"/></svg>',
   o: '<?xml version="1.0" encoding="UTF-8"?><svg class="game-screen-header-icons__oval" width="40" height="40" fill="none" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="m31.97 15.871c0-8.7651-7.1055-15.871-15.871-15.871-8.765 0-15.871 7.1055-15.871 15.871 0 8.7651 7.1055 15.871 15.871 15.871 8.7651 0 15.871-7.1055 15.871-15.871zm-22.336 0c0-3.571 2.8948-6.4658 6.4658-6.4658 3.571 0 6.4658 2.8948 6.4658 6.4658s-2.8948 6.4658-6.4658 6.4658c-3.5709 0-6.4658-2.8948-6.4658-6.4658z" clip-rule="evenodd" fill-rule="evenodd"/></svg>',
 };
+const swapPlayer = {
+  x: "o",
+  o: "x",
+};
 
 let currentPlayer = "o";
-
-let gameBoard;
+let playerOne = "o";
+let gameBoard = ["", "", "", "", "", "", "", "", ""];
+let isSoloGame = true;
 
 function updateTurnIndicator() {
   $turnIndicator.innerHTML =
@@ -53,25 +64,41 @@ function updateTurnIndicator() {
     '<span class="game-screen-header-turn-indicator__title">turn</span>';
 }
 
-function gridCellClick(e) {
-  e.target.removeEventListener("click", gridCellClick);
-  e.target.innerHTML = symbols[currentPlayer];
-  const dataCellId = parseInt(e.target.getAttribute("data-cell-id"));
-  gameBoard[Math.floor(dataCellId / 3)][dataCellId % 3] = currentPlayer;
-  const gameState = isGameFinished();
+function place(position, player) {
+  const $cell = $gridCells[position];
+  $cell.removeEventListener("click", gridCellClick);
+  $cell.innerHTML = symbols[currentPlayer];
+  gameBoard[position] = player;
+  const gameState = isGameFinished(gameBoard);
   if (gameState === "Égalité") {
     console.log("EGALITE");
     scoreTracker["tie"] += 1;
+    $endGameScreen.classList.remove("hidden");
+    $winnerIcon.innerHTML = "round tied";
+    $winnerText.textContent = "";
   } else if (gameState.endsWith("gagne")) {
     const winner = gameState.split(" ")[0];
     $endGameScreen.classList.remove("hidden");
     scoreTracker[winner] += 1;
-    $winnerIcon.innerHTML =
-      "<span>" + symbols[winner] + "</span> takes the round";
+    $winnerIcon.innerHTML = `<span>${symbols[winner]}</span>takes the round`;
+    if (isSoloGame) {
+      if (winner === playerOne) $winnerText.textContent = "You won!";
+      else $winnerText.textContent = "Oh no, you lost...";
+    } else {
+      if (winner === playerOne) $winnerText.textContent = "Player 1 wins!";
+      else $winnerText.textContent = "Player 2 wins!";
+    }
   }
-  if (currentPlayer === "o") currentPlayer = "x";
-  else currentPlayer = "o";
+  currentPlayer = swapPlayer[currentPlayer];
   updateTurnIndicator();
+}
+
+function gridCellClick(e) {
+  const dataCellId = parseInt(e.target.getAttribute("data-cell-id"));
+  place(dataCellId, currentPlayer);
+  if (isSoloGame) {
+    place(predictBestMove(gameBoard, currentPlayer), currentPlayer);
+  }
 }
 
 function resetGrid() {
@@ -79,14 +106,10 @@ function resetGrid() {
     $gridCell.innerHTML = "";
     $gridCell.addEventListener("click", gridCellClick);
   });
-  gameBoard = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
+  gameBoard = ["", "", "", "", "", "", "", "", ""];
 }
 
-function isGameFinished() {
+function isGameFinished(grid) {
   const lines = [
     // Lignes
     [
@@ -137,15 +160,15 @@ function isGameFinished() {
     const [a, b, c] = line;
 
     if (
-      gameBoard[a[0]][a[1]] &&
-      gameBoard[a[0]][a[1]] === gameBoard[b[0]][b[1]] &&
-      gameBoard[a[0]][a[1]] === gameBoard[c[0]][c[1]]
+      grid[a[0]][a[1]] &&
+      grid[a[0]][a[1]] === grid[b[0]][b[1]] &&
+      grid[a[0]][a[1]] === grid[c[0]][c[1]]
     ) {
-      return gameBoard[a[0]][a[1]] + " gagne";
+      return grid[a[0]][a[1]] + " gagne";
     }
   }
 
-  if (gameBoard.flat().every((cell) => cell)) return "Égalité";
+  if (grid.flat().every((cell) => cell)) return "Égalité";
   return "Rien";
 }
 
@@ -153,6 +176,69 @@ function updateScoreTracker() {
   $scoreTrackers.o.textContent = scoreTracker.o;
   $scoreTrackers.x.textContent = scoreTracker.x;
   $scoreTrackers.tie.textContent = scoreTracker.tie;
+}
+
+function evaluateGridValue(grid, turn, steps = 0) {
+  let isWin = isGameFinished(grid);
+  if (isWin.endsWith("gagne"))
+    if (isWin.startsWith(turn)) return { value: 10, steps };
+    else return { value: -10, steps };
+  else if (isWin === "Égalité") return { value: 0, steps };
+  let cells = [];
+  for (let i = 0; i < grid.length; i++) {
+    if (grid[i] === "") {
+      cells.push(i);
+    }
+  }
+
+  const values = cells.map((cell) => {
+    let newGrid = structuredClone(grid);
+    newGrid[cell] = turn;
+    const { value, step } = evaluateGridValue(newGrid, swapPlayer[turn], steps);
+    return { value: -value, steps: step };
+  });
+
+  let max = values[0];
+  values.forEach((value) => {
+    if (
+      value.value > max.value ||
+      (value.value === max.value && value.steps < max.steps)
+    ) {
+      max = value;
+    }
+  });
+  return max;
+}
+
+function predictBestMove(grid, turn) {
+  let cells = [];
+  for (let i = 0; i < grid.length; i++) {
+    if (grid[i] === "") {
+      cells.push(i);
+    }
+  }
+
+  console.log(cells);
+
+  const values = cells.map((cell) => {
+    let newGrid = structuredClone(grid);
+    newGrid[cell] = turn;
+    const { value, steps } = evaluateGridValue(newGrid, swapPlayer[turn]);
+    return { value: -value, steps, cell };
+  });
+
+  console.log(values);
+  let max = values[0];
+  values.forEach((value) => {
+    if (
+      value.value > max.value ||
+      (value.value === max.value && value.steps < max.steps)
+    ) {
+      max = value;
+    }
+  });
+  console.log(max);
+  return max.cell;
 }
 
 $newGameScreenPickPlayerIcons.forEach(function ($newGameScreenPickPlayerIcon) {
@@ -167,6 +253,7 @@ $newGameScreenPickPlayerIcons.forEach(function ($newGameScreenPickPlayerIcon) {
       "new-game-screen-pick-player-icon--selected"
     );
     currentPlayer = $newGameScreenPickPlayerIcon.getAttribute("data-player");
+    playerOne = $newGameScreenPickPlayerIcon.getAttribute("data-player");
 
     console.log(currentPlayer);
   });
@@ -175,6 +262,21 @@ $newGameScreenPickPlayerIcons.forEach(function ($newGameScreenPickPlayerIcon) {
 $newGameScreenNewGameCpuPlayer.addEventListener("click", function () {
   $newGameScreen.classList.add("hidden");
   $gameScreen.classList.remove("hidden");
+  isSoloGame = true;
+  updateTurnIndicator();
+  resetGrid();
+  scoreTracker = {
+    x: 0,
+    o: 0,
+    tie: 0,
+  };
+  updateScoreTracker();
+});
+
+$newGameScreenNewGamePlayer.addEventListener("click", function () {
+  $newGameScreen.classList.add("hidden");
+  $gameScreen.classList.remove("hidden");
+  isSoloGame = false;
   updateTurnIndicator();
   resetGrid();
   scoreTracker = {
